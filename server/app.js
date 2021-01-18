723a553d93acb65a3c6f4603364466873d291ed9
Shubham Kukreti <52024580+Shubham-Kukreti@users.noreply.github.com>
2021-01-18 13:33:55 +0530

    added signup api

--- server/app.js
+++ server/app.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
 const PORT = 5000;
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connection Success.");
+  })
+  .catch((err) => {
+    console.error("Mongo Connection Error", err);
+  });
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); //optional
 
 app.get("/ping", (req, res) => {
   return res.send({
