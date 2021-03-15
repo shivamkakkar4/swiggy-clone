e45ea41ab3525d1eea1f170f58396635d93f08a6
Shivam Kakkar <shivamkakkar4@gmail.com>
2021-03-15 23:57:10 +0530

    Restructured Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import LandingPage from "./Components/LandingPage/LandingPage";
-import Restaurants from "./Components/RestaurantsPage/Restaurants";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import Restaurants from "./Pages/RestaurantsPage/Restaurants";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
